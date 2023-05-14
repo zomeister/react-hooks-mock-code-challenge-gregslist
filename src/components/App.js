bdcbd6558c4ae4fc80175b9f3b02fba693150401
Zoe Guidry <oyezoey@gmail.com>
2023-05-14 18:21:59 -0500

    core deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([])
+
+  useEffect(() => {
+    fetch( "http://localhost:6001/listings")
+    .then( r => r.json() )
+    .then( listings => setListings(listings) )
+  }, [])
+  
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={listings}/>
     </div>
   );
 }
--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,21 +1,49 @@
-import React from "react";
+import {useState} from "react";
 
-function ListingCard() {
+function ListingCard ({id, description, image, location }) {
+  const [favorite, setFavorite] = useState(false)
+  function handleFavoriteClick () {
+    setFavorite(true)
+  }
+  function handleUnfavoriteClick () {
+    setFavorite(false)
+  }
+  function handleDeleteClick () {
+    const baseUrl = "http://localhost:6001/listings/"
+    fetch( `${baseUrl}${id}`, {
+      method: "DELETE",
+    })
+    .then( r => r.json() )
+    .then( () => console.log("DELETEd!") )
+  }
+
+
   return (
     <li className="card">
       <div className="image">
-        <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <span className="price">$0-5</span>
+        <img src={image} alt={description} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
-        ) : (
-          <button className="emoji-button favorite">☆</button>
+        {favorite 
+        ? (<button
+            className="emoji-button favorite active"
+            onClick={handleUnfavoriteClick}>
+            ★
+          </button>)
+        : (<button
+            className="emoji-button favorite"
+            onClick={handleFavoriteClick}>
+            ☆
+          </button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
+        <strong>{description}</strong>
+        <span> · {location}</span>
+        <button
+          className="emoji-button delete"
+          onClick={handleDeleteClick}>
+          🗑
+        </button>
       </div>
     </li>
   );
